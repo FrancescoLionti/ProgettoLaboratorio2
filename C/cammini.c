753d35dead708d81656d1de89d770c54b44d83f6
Francesco Lionti <130184836+FrancescoLionti@users.noreply.github.com>
2025-05-17 21:40:37 +0200

    correzioni finali

--- C/cammini.c
+++ C/cammini.c
@@ -309,15 +309,15 @@ void scriviCammini(int a, int b, nodoAbr *alberoCopertura, int trovato, double t
     switch (trovato)
     {
     case -1: // se la sorgente non appartiene al grafo
-        fprintf(camminiFile, "codice %d non valido", a);
+        fprintf(camminiFile, "codice %d non valido\n", a);
         printf("%s: Codice %d non valido. Tempo di elaborazione:%f \n", strCammino, a, tempo);
         break;
     case -2: // se lad destinazione non appartiene al grafo
-        fprintf(camminiFile, "codice %d non valido", b);
+        fprintf(camminiFile, "codice %d non valido\n", b);
         printf("%s: Codice %d non valido. Tempo di elaborazione:%f \n", strCammino, b, tempo);
         break;
     case 0: // se il cammino non esiste
-        fprintf(camminiFile, "non esistono cammini da %d a %d", a, b);
+        fprintf(camminiFile, "non esistono cammini da %d a %d\n", a, b);
         printf("%s: Nessun cammino. Tempo di elaborazione:%f \n", strCammino, tempo);
         break;
 
@@ -332,7 +332,7 @@ void scriviCammini(int a, int b, nodoAbr *alberoCopertura, int trovato, double t
         if (!cammino)
             xtermina("Errore: allocazione cammino fallita", QUI);
 
-        // riempio l'array di nodi che fanno parte del cammino
+        // riempio l'array di nodi che fanno parte del cammino, quindi ricostruisco il cammino
         ricostruisciCammino(alberoCopertura, a, b, cammino);
 
         // scrivo i cammini nel file
